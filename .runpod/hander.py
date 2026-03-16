6da39880acdb9822bc8dc2386efc7b21db243374
henk717 <7526774+henk717@users.noreply.github.com>
2026-03-17 00:08:50 +0100

    Alright alright, ill make a real fake one

--- .runpod/hander.py
+++ .runpod/hander.py
@@ -1,1 +1,8 @@
-print("Hi! Want to use KoboldCpp on serverless? You can! But it must be a load balanced instance, this is the only thing we support.")
+import runpod
+def handler(event):
+    result = "Hi! Want to use KoboldCpp on serverless? You can! But it must be a load balanced instance, this is the only thing we support."
+    
+    # Return the result
+    return result
+
+runpod.serverless.start({"handler": handler})
